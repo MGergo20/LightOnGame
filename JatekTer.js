cac5cb1dc642ef877ac985717a07b7d2e3df61a2
Bottesz <botyaring@gmail.com>
2025-03-27 10:48:05 +0100

    tartunk valahol

---
+++ JatekTer.js
@@ -0,0 +1,35 @@
+import Elem from "./Elem.js";
+import Info from "./Info.js";
+
+export default class Jatekter{
+    #lista = [" "," "," "," "," "," "," "," "," "];
+    #lampaszamlalo=0;
+    szuloElem;
+    constructor(szuloELem,jatekterelem){
+    this.szuloElem = szuloElem;
+    this.jatekterelem = jatekterelem;
+    this.megjelenit();
+    this.esemenykezelo();
+    }
+}
+
+
+#esemenykezelo(){
+    window.addEventListener("kivalaszt", (event) => {
+        const lampaElem = event.target.closest('.lampa');
+       if (lampaElem) {
+        
+       }
+        this.szuloElem.innerHTML = "";
+        this.#megjelenit();
+        this.#lampaszamlalo++;
+    
+});
+}
+
+#megjelenit(){
+    for (let index = 0; index < #lista.length; index++) {
+        new Elem(this.#lista[index], index, this.szuloElem);
+        
+    }
+}
---
+++ Lampa.js
@@ -0,0 +1,25 @@
+export default class Lampa{
+    #index=0;
+    #adat=" ";
+    #szuloElem;
+    constructor(szuloElem,adat,index){
+        this.#szuloElem=szuloElem;
+        this.#adat=adat;
+        this.#index=index;
+        this.megjelenit();
+        this.elem = document.querySelector(".elem:last-child");
+        this.elem.addEventListener("click", ()=> {
+            if (this.#adat === " "){
+            const esemeny = new CustomEvent("kivalaszt",{detail:this.#index});
+            window.dispatchEvent(esemeny);
+    }
+});
+    }
+    megjelenit(){
+        let html = `<div class= "elem">
+                    ${this.#adat}
+                    </div>`
+    this.#szuloElem.insertAdjacentHTML("beforeend",html);
+}
+
+    }
